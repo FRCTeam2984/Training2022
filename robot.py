2416fe6586eb80ddea0a5f84d3666a61f33368d7
team2984 <team2984Git@gmail.com>
2021-12-14 17:44:03 -0800

    add utils, imu

--- robot.py
+++ robot.py
@@ -1,18 +1,24 @@
 import wpilib
-from subsystems import drive
+from subsystems import drive, drive_imu
+from utils import ID
 
 class MyRobot(wpilib.TimedRobot):
 
    def robotInit(self):
-      self.drive = drive.Drive()
-      self.left_drivestick = wpilib.Joystick(0)
-      self.right_drivestick = wpilib.Joystick(1)
+      self.drive = drive.Drive(ID.DRIVE_LEFT_FRONT, ID.DRIVE_LEFT_BACK, ID.DRIVE_RIGHT_FRONT, ID.DRIVE_RIGHT_BACK)
+      self.imu = drive_imu.DriveImu(ID.DRIVE_IMU)
+      self.left_drivestick = wpilib.Joystick(ID.DRIVE_LEFT_JOYSTICK)
+      self.right_drivestick = wpilib.Joystick(ID.DRIVE_RIGHT_JOYSTICK)
+      self.back_limit_switch = wpilib.DigitalInput(ID.LIMIT_SWITCH_NC)
    
    def teleopInit(self):
       pass
 
    def teleopPeriodic(self):
-      self.drive.customDrive(self.left_drivestick.getX(), self.left_drivestick.getY(), self.right_drivestick.getX(), self.right_drivestick.getY())
+      try:
+         self.drive.customDrive(self.left_drivestick.getX(), self.left_drivestick.getY(), self.right_drivestick.getX(), self.right_drivestick.getY())
+      except:
+         raise
 
 if __name__ == "__main__":
    wpilib.run(MyRobot)
