1b24f3dddc2326ae144ec4c6279c7fd32626276d
yello12 <brickbeacon123@gmail.com>
2021-12-24 12:00:20 -0800

    add centralDrive boilerplate

--- robot.py
+++ robot.py
@@ -1,29 +1,48 @@
-import wpilib
-from subsystems import drive, drive_imu
+import wpilib, ctre
 from utils import ID
+from subsystems import drive
+from commands import turn_to_angle
 
 class MyRobot(wpilib.TimedRobot):
 
    def robotInit(self):
-      self._drive = drive.Drive(ID.DRIVE_LEFT_FRONT, ID.DRIVE_LEFT_BACK, ID.DRIVE_RIGHT_FRONT, ID.DRIVE_RIGHT_BACK)
-      self.imu = drive_imu.DriveImu(self._drive.backRight)
-      self.left_drivestick = wpilib.Joystick(ID.DRIVE_LEFT_JOYSTICK)
-      self.right_drivestick = wpilib.Joystick(ID.DRIVE_RIGHT_JOYSTICK)
+      #components: These are classes representing all the electrical sensors and actuators on the robot.
+      self.frontLeft = ctre.WPI_TalonSRX(ID.DRIVE_LEFT_FRONT)
+      self.backLeft = ctre.WPI_TalonSRX(ID.DRIVE_LEFT_BACK)
+
+      self.frontRight = ctre.WPI_TalonSRX(ID.DRIVE_RIGHT_FRONT)
+      self.backRight = ctre.WPI_TalonSRX(ID.DRIVE_RIGHT_BACK)
+
+      self.drive_imu = ctre.PigeonIMU(self.backRight)
       self.back_limit_switch = wpilib.DigitalInput(ID.LIMIT_SWITCH_NC)
 
+      # Might change to XBOX controller depending on it working or not.
+      self.drive_stick = wpilib.Joystick(ID.DRIVE_JOYSTICK)
+      self.operator_stick = wpilib.XboxController(ID.OPERATOR_JOYSTICK)
+
+      #subsystems: These combine multiple components into a coordinated system.
+      self._drive = drive.Drive(self.frontLeft, self.backLeft, self.frontRight, self.backRight, self.drive_imu)
+      
+      #commands: These utilize subsystems to perform autonomous routines.
+
    def teleopInit(self):
-      pass
+      self.frontLeft.setInverted(False)
+      self.backLeft.setInverted(False)
+      self.frontRight.setInverted(False)
+      self.backRight.setInverted(False)
       
    def teleopPeriodic(self):
+      # Exceptions are used to not crash robot code if in competition, 
+      # but in our testing case we are raising the exceptions because we want to debug.
       try:
-         self._drive.customDrive(self.left_drivestick.getX(), self.left_drivestick.getY(), self.right_drivestick.getX(), self.right_drivestick.getY())
+         if self.drive_stick.getTop() == True:
+            self._drive.arcadeDrive(self.drive_stick.getY(), self.drive_stick.getX())
+         else:
+            self._drive.centralDrive(self.drive_stick.getZ(), self.drive_stick.getY(), self.drive_stick.getX())
+            pass
       except:
          raise
          
-      try:
-         print(self.imu.getYaw())
-      except:
-         raise
          
 
 if __name__ == "__main__":
--- subsystems/drive.py
+++ subsystems/drive.py
@@ -1,13 +1,19 @@
-import wpilib, ctre
+from ctre import WPI_TalonSRX, PigeonIMU
+import math
 
+# parameter : type
 class Drive:
-   def __init__(self, frontLeft, backLeft, frontRight, backRight):
-      self.frontLeft = ctre.WPI_TalonSRX(frontLeft)
-      self.backLeft = ctre.WPI_TalonSRX(backLeft)
+   #CONTRUCTOR
+   def __init__(self, _frontLeft : WPI_TalonSRX, _backLeft : WPI_TalonSRX, _frontRight : WPI_TalonSRX, _backRight : WPI_TalonSRX, _drive_imu : PigeonIMU):
+      self.frontLeft = _frontLeft
+      self.backLeft = _backLeft
 
-      self.frontRight = ctre.WPI_TalonSRX(frontRight)
-      self.backRight = ctre.WPI_TalonSRX(backRight)
+      self.frontRight = _frontRight
+      self.backRight = _backRight
+      
+      self.drive_imu = _drive_imu
 
+   #HELPER FUNCTIONS
    def setRightSpeed(self, speed):
       # speed is a float value from -1 to 1
       speed = max(-1, min(speed, 1))
@@ -24,7 +30,28 @@ class Drive:
       self.setLeftSpeed(left)
       self.setRightSpeed(right)
 
-   def customDrive(self, left_x, left_y, right_x, right_y):
-      left_speed = left_y
-      right_speed = right_y
+   def getRotation(self):
+        # Get the yaw from the IMU
+        return self.drive_imu.getYawPitchRoll()[1]
+
+   def getYaw(self):
+        return self.getRotation()[0]
+
+   #DRIVE FUNCTIONS
+   def arcadeDrive(self, y, x):
+      left_speed = y + x
+      right_speed = y - x
       self.setSpeed(left_speed, right_speed)
+
+   def centralDrive(self, speed, y, x):
+      # speed is a float value from -1 to 1
+      cur_rotation = self.getYaw()
+      # Using atan2, it assures that only the shortest way will be used to get to the desired angle and not the 
+      # long route possibly recieved by using just the math.atan function.
+      end_rotation = math.atan2(y, x)
+      # Use PID or something in this next step idk
+      left_speed = speed
+      right_speed = speed
+      self.setSpeed(left_speed, right_speed)
+      # this function is NOT finished. It should be based on a two axis joystick determing rotation and one 
+      # joystick controlling the speed. The motors should rotate to the angle a two axis joystick points
